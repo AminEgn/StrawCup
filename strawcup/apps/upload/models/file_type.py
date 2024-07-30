8729e5cbcc1a05cdcfae0245ece4e85dd597f43d
amin-egn <aminx15am@yahoo.com>
2024-07-30 16:56:50 +0330

    #file_type.py ~ apps/upload/models/ -> change type_name to extension also modify __str__ method

--- strawcup/apps/upload/models/file_type.py
+++ strawcup/apps/upload/models/file_type.py
@@ -5,9 +5,9 @@ from django.db import models
 class FileType(models.Model):
     """File Type"""
 
-    type_name = models.CharField(max_length=10)
+    extension = models.CharField(max_length=10)
     mime_type = models.CharField(max_length=110)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.type_name
+        return self.extension
