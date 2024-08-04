38f98f3e158deb953d037a5c27639beff961cdbe
amin-egn <aminx15am@yahoo.com>
2024-08-04 16:38:23 +0330

    #session.py ~ apps/upload/models/ -> 1) add status CharField also add Status choices for it 2) add default uuid4 for hash_serial 3) add default for last_used (timezone.now) 4) add related_name=session, null=True and blank=True for file fk 5) add null=True and blank=True to user 6) add is_active property method

--- strawcup/apps/upload/models/session.py
+++ strawcup/apps/upload/models/session.py
@@ -1,21 +1,52 @@
+# std
+import uuid
+from datetime import timedelta
+
 # dj
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 
 class Session(models.Model):
     """Session"""
 
-    hash_serial = models.CharField(max_length=70, unique=True)
+    class Status(models.TextChoices):
+        CREATED = "created"
+        PROCESSING = "processing"
+        COMPLETED = "completed"
+        FAILED = "failed"
+        DELETED = "deleted"
+
+    status = models.CharField(
+        max_length=15, choices=Status.choices, default=Status.CREATED
+    )
+    hash_serial = models.CharField(max_length=70, unique=True, default=uuid.uuid4)
     chunked = models.BooleanField(default=False)
     file_size = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now=True)
-    last_used = models.DateTimeField()
+    last_used = models.DateTimeField(default=timezone.now)
 
-    file = models.OneToOneField("File", on_delete=models.CASCADE)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sessions"
+    file = models.OneToOneField(
+        "File", on_delete=models.CASCADE, related_name="session", null=True, blank=True
     )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="sessions",
+        # null and blank true has been set because there is no authentication for views
+        null=True,
+        blank=True
+    )
+
+    @property
+    def is_active(self):
+        # static seven days later
+        seven_days = timedelta(days=7)
+        if self.created_at + seven_days >= timezone.now():
+            return True
+
+        return False
 
     def __str__(self):
         return self.hash_serial
