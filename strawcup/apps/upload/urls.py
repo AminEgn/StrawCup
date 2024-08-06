8d63c9eef5d369beb49f2b944c6aa7849e2e83bc
amin-egn <aminx15am@yahoo.com>
2024-08-06 16:47:40 +0330

    #urls.py ~ apps/upload/ -> edit path: remove process

--- strawcup/apps/upload/urls.py
+++ strawcup/apps/upload/urls.py
@@ -6,7 +6,7 @@ from apps.upload import views
 
 
 urlpatterns = [
-    path("process", views.FileUploadView.as_view(), name="upload_process"),
+    path("", views.FileUploadView.as_view(), name="upload_process"),
     path("session_init", views.SessionInitView.as_view(), name="upload_session_init"),
     path(
         "session_process/<slug:hash_serial>",
