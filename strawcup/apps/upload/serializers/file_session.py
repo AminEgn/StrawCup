9ac39335203c802e4e7111ddad108be2ac2604b6
amin-egn <aminx15am@yahoo.com>
2024-08-11 11:11:22 +0330

    #file_session.py ~ apps/upload/serializers/ -> 1) change SessionSerializer parent to Serializer 2) add name, file_size, hash_serial and created_at as serializer field 3) override create method

--- strawcup/apps/upload/serializers/file_session.py
+++ strawcup/apps/upload/serializers/file_session.py
@@ -1,27 +1,41 @@
+# std
+import uuid
+
+# dj
+from django.core.files.base import ContentFile
+from django.core.validators import RegexValidator
+
 # drf
 from rest_framework import serializers
 
 # internal
-from apps.upload.models import Session
+from apps.upload.models import Session, File
 
 
-class SessionSerializer(serializers.ModelSerializer):
+class SessionSerializer(serializers.Serializer):
     """Session Serializer"""
 
-    user = serializers.CurrentUserDefault()
+    # user can specify the file's name.
+    # it should validate with regex (it must be a name of a file with extension)
+    name = serializers.CharField(
+        max_length=255, write_only=True, validators=[RegexValidator("\w+[.]{1}[a-zA-Z]+")]
+    )
+    file_size = serializers.IntegerField()
+    hash_serial = serializers.CharField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    # user = serializers.HiddenField(
+    #     default=serializers.CurrentUserDefault()
+    # )
 
-    class Meta:
-        model = Session
-        fields = (
-            "hash_serial",
-            "chunked",
-            "file_size",
-            "created_at",
-            "user",
-        )
-        read_only_fields = (
-            "hash_serial",
-        )
-        extra_kwargs = {
-            "user": {"required": False, "write_only": True},
-        }
+    def create(self, validated_data):
+        name = validated_data.pop("name", "")
+        # in session initialize, user has to fill file name
+        hs = str(uuid.uuid4())
+        validated_data["hash_serial"] = hs
+        # user = validated_data["user"]
+        # the user has to pass tp file object
+        content_file = ContentFile(b"", name=name)
+        f = File.objects.create(name=name, f=content_file)
+        validated_data["file"] = f
+        session = Session.objects.create(**validated_data)
+        return session
